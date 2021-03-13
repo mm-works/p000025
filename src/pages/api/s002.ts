bd93710fb69c6eed41d9e010d7f3e710cef79c39
taoqf <tao_qiufeng@126.com>
2021-03-13 10:51:00 +0800

    remove files

--- src/pages/api/s002.ts
+++
@@ -1,2 +0,0 @@
-export { default, config } from './d001/pg007/s002';
-export type { r002 } from './d001/pg007/s002';
--- src/pages/api/s003.ts
+++
@@ -1,2 +0,0 @@
-export { default, config } from './d002/pg001/s002';
-export type { r002 as r003 } from './d002/pg001/s002';
